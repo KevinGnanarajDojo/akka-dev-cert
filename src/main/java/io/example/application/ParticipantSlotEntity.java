87f57f4a2c8bec65da0639377c328366e5715804
KevinGnanarajDojo <kevin.gnanaraj@dojo.tech>
2026-03-20 11:55:03 +0000

    feat: initial Participant Slot Entity implementation

--- src/main/java/io/example/application/ParticipantSlotEntity.java
+++ src/main/java/io/example/application/ParticipantSlotEntity.java
@@ -12,22 +12,30 @@ public class ParticipantSlotEntity
 
         public Effect<Done> unmarkAvailable(ParticipantSlotEntity.Commands.UnmarkAvailable unmark) {
                 // Supply your own implementation
-                return effects().reply(Done.done());
+                return effects()
+                        .persist(new Event.UnmarkedAvailable(unmark.slotId(), unmark.participantId(), unmark.participantType()))
+                        .thenReply(__ -> Done.done());
         }
 
         public Effect<Done> markAvailable(ParticipantSlotEntity.Commands.MarkAvailable mark) {
                 // Supply your own implementation
-                return effects().reply(Done.done());
+                return effects()
+                        .persist(new Event.MarkedAvailable(mark.slotId(), mark.participantId(), mark.participantType()))
+                        .thenReply(__ -> Done.done());
         }
 
         public Effect<Done> book(ParticipantSlotEntity.Commands.Book book) {
                 // Supply your own implementation
-                return effects().reply(Done.done());
+                return effects()
+                        .persist(new Event.Booked(book.slotId(), book.participantId(), book.participantType(), book.bookingId()))
+                        .thenReply(__ -> Done.done());
         }
 
         public Effect<Done> cancel(ParticipantSlotEntity.Commands.Cancel cancel) {
                 // Supply your own implementation
-                return effects().reply(Done.done());
+                return effects()
+                        .persist(new Event.Canceled(cancel.slotId(), cancel.participantId(), cancel.participantType(), cancel.bookingId()))
+                        .thenReply(__ -> Done.done());
         }
 
         record State(
@@ -79,8 +87,13 @@ public class ParticipantSlotEntity
         }
 
         @Override
-        public ParticipantSlotEntity.State applyEvent(ParticipantSlotEntity.Event event) {
+        public State applyEvent(ParticipantSlotEntity.Event event) {
                 // Supply your own implementation
-                return null;
+                return switch(event){
+                        case Event.MarkedAvailable e -> new State(e.slotId(), e.participantId(), e.participantType(), "available");
+                        case Event.UnmarkedAvailable e -> new State(e.slotId(), e.participantId(), e.participantType(), "unavailable");
+                        case Event.Booked e -> new State(e.slotId(), e.participantId(), e.participantType(), "booked");
+                        case Event.Canceled e -> new State(e.slotId(), e.participantId(), e.participantType(), "canceled");
+                };
         }
 }
